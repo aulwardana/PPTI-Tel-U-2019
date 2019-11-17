cea5fe3ce0d440280ff9b5a1c6b233027864d00c
Aul Wardana <auldesain@gmail.com>
2019-11-17 12:44:37 +0700

    cleaning boilprate

--- Fingerprint/Enroll/new_gui_enroll.py
+++ Fingerprint/Enroll/new_gui_enroll.py
@@ -31,32 +31,23 @@ validate = frame.register(validate_input)
 for i in range(10):
     txt = tk.Entry(frame, font=large_font,width=15,validate="key", validatecommand=(validate, "%P"))
 
-# txt = tk.Entry(frame,font=large_font,width=15)
 inputStr = txt
 txt.pack()
 
-#--------------------------------------------------------------------
 def numInser(value):
 
-    # inform function to use external/global variable
     global pin
 
     if value == 'Del':
-        # remove last number from `pin`
         pin = pin[:-1]
-        # remove all from `entry` and put new `pin`
         txt.delete('0', 'end')
         txt.insert('end', pin)
 
     else:
-        # add number to pin
         pin += value
-        # add number to `entry`
         txt.insert('end', value)
 
     print("Current:", pin)
-
-# --- main ---
 
 keys = [
     ['1', '2', '3'],    
@@ -64,22 +55,19 @@ keys = [
     ['7', '8', '9'],    
     ['', '0','Del'],    
 ]
-# create global variable for pin
-pin = '' # empty string
+
+pin = ''
 
 root = tk.Tk()
 e = tk.Entry(root)
 frame.grid(row=0, column=0, columnspan=3, ipady=5)
-# create buttons using `keys`
+
 for y, row in enumerate(keys, 5):
     for x, key in enumerate(row):
-        # `lambda` inside `for` has to use `val=key:numInser(val)` 
-        # instead of direct `numInser(key)`
         b = tk.Button(root, text=key, command=lambda val=key:numInser(val), width=10, font=medium_font)
         b.grid(row=y, column=x, ipadx=10, ipady=10)
-#--------------------------------------------------------------------
+
 def clicked():
-    # tk.Tk()
     messagebox.showinfo('Finger system', 'Tekan OK, Lalu letakkan jari anda')
     res = txt.get()
     ard.write(str(res).encode())
